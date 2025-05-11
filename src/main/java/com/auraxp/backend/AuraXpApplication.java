0e0a36f12f2210bd611182a0cdbecdc315d7cc39
Drew <tullos.andrew@gmail.com>
2025-05-11 12:53:29 -0500

    Init

---
+++ src/main/java/com/auraxp/backend/AuraXpApplication.java
@@ -0,0 +1,13 @@
+package com.auraxp.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuraXpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuraXpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/auraxp/backend/AuraXpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auraxp.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuraXpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
